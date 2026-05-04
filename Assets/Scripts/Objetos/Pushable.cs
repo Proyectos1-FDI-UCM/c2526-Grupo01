01435555ad2a239073d97719c9a6c165c19de6ab
Hector Prous <hprous@ucm.es>
2026-05-04 18:17:48 +0200

    Caja arreglada, logica con triggers
    He añadido a las cajas una nueva logica con triggers, de esta forma ya no se utilizan ni layers ni raycast

--- Assets/Scripts/Objetos/Pushable.cs
+++ Assets/Scripts/Objetos/Pushable.cs
@@ -10,7 +10,6 @@ using UnityEngine;
 
 // Añadir aquí el resto de directivas using
 
-
 /// <summary>
 /// Antes de cada class, descripción de qué es y para qué sirve,
 /// usando todas las líneas que sean necesarias.
@@ -18,148 +17,90 @@ using UnityEngine;
 
 public class Pushable : MonoBehaviour
 {
-    [Header("Detección")]
-
-    //distancia del raycast que detecta al jugador
-    [SerializeField] private float raycastDistance = 3f;
-    //distancia del raycast cuando se interactua (para que no se escape facilmente)
-    [SerializeField] private float raycastCage = 5f;
-    //layer que detecta el raycast como jugador (el resto las ignora)
-    [SerializeField] private LayerMask playerLayer;
-
     [Header("Referencias")]
-    //Imagen de boton de interaccion
     [SerializeField] private GameObject interactIcon;
 
-    //referencias 
+    [Header("Velocidad")]
+    [SerializeField] private float velBox = 100f; //parametro que determina en % la velocidad que tendra el jugador al agarrar la caja 
+
     private Rigidbody2D rb;
     private PlayerMovement player;
-
-    //booleano que se activa al interactuar con el pushable
     private bool isBeingPushed;
-    //creaccion de raycast
-    private float trueRaycast;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        trueRaycast = raycastDistance;
     }
 
-    //Update solo se encarga de detectar al jugador cada frame 
     private void Update()
-    {   
-        DetectPlayer();
+    {
+        //logica del boton interact, si InteractWasPressedThisFrame activa el bool y si se vuelve a clickar lo desactiva
+        if (player != null)
+        {
+            if (InputManager.Instance.InteractWasPressedThisFrame())
+            {
+                if (!isBeingPushed)
+                {
+                    isBeingPushed = true;
+                }
+                else
+                {
+                    isBeingPushed = false;
+                }
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        //si el jugador interactua con el objeto (pulsa E):
-        if (isBeingPushed && player != null)
+        //para que funcione en las pausas (eso creo, no lo he probado mucho y se lo mire a samu)
+        if (Physics2D.simulationMode == SimulationMode2D.Script)
         {
-            //Descongela X para poder moverla
-            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-            rb.linearVelocity = new Vector2(player.GetCurrentVelX(), rb.linearVelocity.y);
-
-
-            //hacemos raycast mas grande para que el jugador no salga
-            trueRaycast = raycastCage;
         }
         else
-        {
-            //Congela X para que no se pueda mover
-            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-            rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
-
-            //devolvemos raycast al tamaño natural 
-            trueRaycast = raycastDistance;
-        }
-    }
-
-    private void DetectPlayer()
-    {
-        //deteccion de la posicion del jugador respecto a la caja (izq o drch)
-        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, trueRaycast, ~LayerMask.GetMask("Suelo"));
-        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, trueRaycast, ~LayerMask.GetMask("Suelo"));
-
-        //asignamos el hit al rayo que haya detectado al jugador (esto puede ayudar en un futuro si queremos saber
-        //en que lado el jugador cogio la caja, de momento es irrelevante)
-        RaycastHit2D hit;
-        if (hitRight.collider != null)
-        {
-            hit = hitRight;
-        }
-        else
-        {
-            hit = hitLeft;
+        {   //logica de movimiento de la caja, si isbeingpushed es true le asigna los mismos valores de velocidad en x que tiene el player
+            if (isBeingPushed && player != null)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb.linearVelocity = new Vector2(player.GetCurrentVelX() * (velBox / 100f), rb.linearVelocity.y);
+            }
+            else
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+                rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+            }
         }
         
-        //detectamos al jugador perse y obtenemos su componente de playerMovement
-        if (hit.collider != null)
-        {
-            PlayerMovement deteccion = hit.collider.GetComponentInParent<PlayerMovement>();
-            
-            //cambiamos variable y activamos el icono interactuable
-            if (deteccion != null)
-            {
-                player = deteccion;
-                interactIcon.SetActive(true);
-
-                // Pulsar E agarra, volver a pulsar suelta
-                if (InputManager.Instance.InteractWasPressedThisFrame())
-                {
-                    if (!isBeingPushed)
-                    {
-                        isBeingPushed = true;
-                        player.SetGrabbing(true);
-                    }
-                    else
-                    {
-                        PararPush();
-                    }
-                }
-            }
-        }
-        else
-        {
-            // El jugador se alejo soltamos automáticamente el pushable
-            if (isBeingPushed)
-            {
-                PararPush();
-            }
-            //se "olvida" la referencia del jugador
-            player = null;
-            //desactivamos el icono
-            interactIcon.SetActive(false);
-        }
-
-     /*   if (hit.collider != null)
-        {
-            Debug.Log("golpeando a: " + hit.collider.name);
-        }
-        else
-        {
-            Debug.Log("no golpea nadaaa");
-        }*/
-
     }
 
-    //metodo responsable de detener la accion de empuje a traves de doble E o de alejarse
-    private void PararPush()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        isBeingPushed = false;
+        //si lo que entra al trigger posee playermovement lo asigna a una variable, y si esa variable no es nula se la asigna a la variable de clase playermovement y activa el icono de grab
+        PlayerMovement detectedPlayer = other.GetComponent<PlayerMovement>();
 
-        if (player != null)
+        if (detectedPlayer != null)
         {
-            player.SetGrabbing(false);
+            player = detectedPlayer;
+            interactIcon.SetActive(true);
         }
     }
-
-    //vista del rayo en el editor, para facil modificación
-    private void OnDrawGizmosSelected()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * trueRaycast);
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.left * trueRaycast);
+        //la logica es similar a la de ontriggerenter, pero a causa de un bug sin resolver que envolvia un falso trigger, al final se detecta si el jugador esta 
+        //empujando o no para luego sacar la distancia entre la caja y el player y solo si es algo alta permite abandonar la accion de empujar
+        //(antes al intentar agarrar y tirar hacia atras la caja desde muyyy cerca te sacaba inmediatamente por lo que creo que es un problema que detectaba el colider de la caja como trigger)
+        PlayerMovement detectedPlayer = other.GetComponent<PlayerMovement>();
+
+        if (detectedPlayer != null)
+        {
+            float bugFalsoTrigger = Vector2.Distance(transform.position, detectedPlayer.transform.position);
+
+            if (!isBeingPushed || bugFalsoTrigger > 4f)
+            {
+                isBeingPushed = false;
+                player = null;
+                interactIcon.SetActive(false);
+            }
+        }
     }
 }
